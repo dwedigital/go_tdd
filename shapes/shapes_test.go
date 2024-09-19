2d82d3ab857b959a1caee2347e553b8d1adb4439
dwedigital <dave@dwedigital.com>
2024-09-19 20:44:41 +0100

    added interface

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,9 +3,17 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
 	t.Run("perimeter", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Perimeter(rectangle)
+		got := rectangle.Perimeter()
 		want := 40.0
 
 		if got != want {
@@ -14,22 +22,12 @@ func TestPerimeter(t *testing.T) {
 	})
 	t.Run("area", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, 72.0)
 
 	})
 
 	t.Run("area of circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
