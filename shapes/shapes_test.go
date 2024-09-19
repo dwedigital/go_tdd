a2072c0a123327b2d06d8cb1b00ee8eb744880a2
dwedigital <dave@dwedigital.com>
2024-09-19 20:53:47 +0100

    added triangle and re-factored

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -23,15 +23,22 @@ func TestPerimeter(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+
 	t.Run("area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
+		areaTests := []struct {
+			shape Shape
+			want  float64
+		}{
+			{Rectangle{12, 6}, 72.0},
+			{Circle{10}, 314.1592653589793},
+			{Triangle{12, 6}, 36.0},
+		}
 
-	})
+		for _, tt := range areaTests {
 
-	t.Run("area of circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
+			checkArea(t, tt.shape, tt.want)
+
+		}
 	})
 }
 
